8c82fb174ea73d541082b08a11d64962461c4c00
Bishal <hapiebishal5@gmail.com>
2024-10-09 22:50:33 +0545

    update

--- src/component/Form.js
+++ src/component/Form.js
@@ -8,6 +8,14 @@ export default function Form(props) {
     const handleonchange=(event)=>{
         setText(event.target.value)
     }
+    const handleLowClick=()=>{
+        const newText=text.toLowerCase();
+        setText(newText)
+    }
+    const handleRvClick=()=>{
+        const newText=text.split(/[ ]+/)
+        setText(newText.join(""))
+    }
     const[text,setText]=useState("Enter your Text")
   return (
       <div className='container'>
@@ -17,6 +25,8 @@ export default function Form(props) {
             <textarea className="form-control" value={text} id="mybox" onChange={handleonchange} rows="8"></textarea>
         </div> 
     <button className="btn btn-primary" onClick={handleonClick}>Change to upperCase</button>
+    <button className="btn btn-primary" onClick={handleLowClick}>Change to LowerCase</button>
+    <button className="btn btn-primary" onClick={handleRvClick}>Change to RemoveSpace</button>
   </div>
     
   )
